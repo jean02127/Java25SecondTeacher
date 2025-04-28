131d07d71956423a0e62115ba54e789767438b7f
jean <jinx0727@gmail.com>
2025-04-28 16:24:15 +0800

    0428_final

--- src/MethodInfo.java
+++ src/MethodInfo.java
@@ -1,19 +1,19 @@
 public class MethodInfo {
     public static void main(String[] args) throws Exception {
         // 沒有輸入參數 ＆ 沒有回傳值
-        // sayHello1();
+        sayHello1();
 
         // 有輸入參數 ＆ 沒有回傳值
-        // String inputName = "David";
-        // int[] score = {55};
-        // sayHello2(inputName, 10, score);
-        // int[] score2 = {65};
-        // sayHello2("Tom", 20, score2);
+        String inputName = "David";
+        int[] score = {55};
+        sayHello2(inputName, 10, score);
+        int[] score2 = {65};
+        sayHello2("Tom", 20, score2);
 
         // 有輸入參數 ＆ 有回傳值
         // 需要用變數接回傳值
-        // String result = sayHello3("Jerry");
-        // System.out.println(result);
+        String result = sayHello3("Jerry");
+        System.out.println(result);
 
     
     /*
@@ -27,9 +27,9 @@ public class MethodInfo {
      * 其他 印出(ok)
      */
 
-    String getResult = checkScore(85);
-    System.out.println(getResult);
-    // System.out.println(checkScore(85));
+    // String getResult = checkScore(85);
+    // System.out.println(getResult);
+    System.out.println(checkScore(85));
         
     }
 
@@ -42,11 +42,11 @@ public class MethodInfo {
      * 方法名稱: sayHello1
      * 輸入參數: 無
      */
-    // public static void sayHello1() {
-    //     System.out.println("Hi everyone");
-    //     // void 可省略 return;
-    //     return;
-    // }
+    public static void sayHello1() {
+        System.out.println("Hi everyone");
+        // void 可省略 return;
+        return;
+    }
 
     /*
      * 修飾 靜態? 回傳型態 方法名稱(輸入參數)
@@ -57,13 +57,13 @@ public class MethodInfo {
      * 方法名稱: sayHello2
      * 輸入參數: String name
      */
-    // public static void sayHello2(String name, int age, int[] score) {
-    //     System.out.println("Hi " + name);
-    //     System.out.println("Age: " + age);
-    //     System.out.println("Score[0]: " + score[0]);
-    //     // void 可省略 return;
-    //     return;
-    // }
+    public static void sayHello2(String name, int age, int[] score) {
+        System.out.println("Hi " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Score: " + score[0]);
+        // void 可省略 return;
+        return;
+    }
 
     /*
      * 修飾 靜態? 回傳型態 方法名稱(輸入參數)
@@ -71,12 +71,12 @@ public class MethodInfo {
      * 修飾: public, private, protected
      * 靜態: static (靜態方法只能呼叫靜態方法)
      * 有回傳值: 型態為: String
-     * 方法名稱: sayHello2
+     * 方法名稱: sayHello3
      * 輸入參數: String name
      */
-    // public static String sayHello3(String name) {
-    //     return "Hi " + name;
-    // }
+    public static String sayHello3(String name) {
+        return "Hi " + name;
+    }
 
 
     public static String checkScore(int score) {
